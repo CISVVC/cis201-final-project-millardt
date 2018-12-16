c5928bc045f6ea14a94bea23a595a73601da6650
troy millard <troynagle@cis.vvc.edu>
2018-12-15 23:57:59 -0800

    update

---
+++ nogood.cpp
@@ -0,0 +1,29 @@
+/*
+File name:
+Description:
+Author: Troy Millard
+Email: naglet@student.vvc.edu
+*/
+
+#include<iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+
+{
+cout << "that's too bad, it's like a minute away from 11:55 and I couldn't seem to get it\n" <<
+"Either way, I still gave it a shot and put as much as I could muster into the final\n" <<
+"it seems like it would have been easier if I had the notes from the day you went over it\n" <<
+"unfortunately I lost them XD\n" <<
+"also I couldn't seem to find any provided templates for it either\n" <<
+"so I guess instead of having 70% of the project done I tried to do the whole thing\n" << 
+"I even provided my own alice.text from gutenburg lol\n" <<
+"oh well though I tried and went till the last minute sorry I didn't finish\n" << 
+"\n" <<
+"Later then, see ya in the next class";
+
+  return 0;
+}
